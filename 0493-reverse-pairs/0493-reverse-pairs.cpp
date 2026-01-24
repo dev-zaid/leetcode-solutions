66eb142c9c42946e06dd7ad8f2ccfb570e32e25a
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-01-24 15:27:41 +0400

    Time: 592 ms (6.94%), Space: 243.5 MB (29.74%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int merge(vector<int>& arr, int start, int mid, int end){
+        int count = 0, i=start, j=mid+1;
+        for(int i=start; i<=mid; i++){
+            while(j<=end && arr[i] > 2LL *arr[j]){
+                j++;
+            }
+
+            count += j-(mid+1);
+        }
+
+        vector<int> temp;
+        i=start, j=mid+1;
+        while(i<=mid && j<=end){
+            if(arr[i] < arr[j]){
+                temp.push_back(arr[i]);
+                i++;
+            }
+
+            else{
+                temp.push_back(arr[j]);
+                j++;
+            }
+        }
+
+        while(i<=mid){
+            temp.push_back(arr[i]);
+            i++;
+        }
+
+        while(j<=end){
+            temp.push_back(arr[j]);
+            j++;
+        }
+
+        for(int idx=0; idx<temp.size(); idx++){
+            arr[idx+start] = temp[idx];
+        }
+
+        return count;
+    }
+
+    int mergeSort(vector<int>& arr, int start, int end){
+        int inv = 0;
+
+        if(start<end){
+            int mid = start + (end-start)/2;
+            inv += mergeSort(arr, start, mid);
+            inv += mergeSort(arr, mid+1, end);
+            inv += merge(arr, start, mid, end);
+        }
+
+        return inv;
+    }
+
+    int reversePairs(vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size()-1);
+    }
+};
