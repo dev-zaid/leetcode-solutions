e3f471570fdabc579bc00d62f1ab6407ff68b27b
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-01-28 18:51:07 +0400

    Time: 2 ms (43.17%), Space: 40.3 MB (43.35%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -5,17 +5,17 @@ public:
         vector<int> ans(n);
 
         ans[0] = 1;
-        
+
         // Prefix Product of each index in the answer array
         for(int i=1; i<n; i++){
             ans[i] = ans[i-1] * arr[i-1];
         }
 
-        int suffix = arr[n-1];
+        int suffix = 1;
 
         // Suffix Product of each index in the answer array
-        for(int i=n-2; i>=0; i--){
-            ans[i] = ans[i] * suffix;
+        for(int i=n-1; i>=0; i--){
+            ans[i] *= suffix;
             suffix *= arr[i];
         }
 
