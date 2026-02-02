f15e0820591f63e0cab61619456b0608be0b353c
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-02-02 17:22:22 +0400

    Time: 3 ms (79.44%), Space: 11.7 MB (63.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int lp = 0, res=0, n = s.length();
-        unordered_map<char, int> characterMap;
+        vector<int> characterMap(256, -1);
 
         for(int i=0; i<s.length(); i++){
             // Find and update the left pointer to the index after the previously occured char
-            if(characterMap.find(s[i]) != characterMap.end()){
+            if(characterMap[s[i]] != -1){
                 lp = max(lp, characterMap[s[i]] + 1);
-                cout << lp << "," << s[i] << endl;
             }
 
             // Add index of the character to the map
             characterMap[s[i]] = i;
 
-            cout << "resBefore: " << res;
             res = max(res, i-lp+1);
-            cout << ", resAfter: " << res << endl;
         }
 
         return res;
