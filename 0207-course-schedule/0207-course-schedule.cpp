b1e20e9fd78e9eec0c4782f5756a235693da3d90
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-04-13 00:18:28 +0400

    Time: 0 ms (100%), Space: 19.2 MB (81.55%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-    bool isCyclicDFS(int src,vector<bool>& vis, vector<bool>& recPath,vector<vector<int>>& edges){
+    bool isCyclicDFS(int src, vector<bool>& vis, vector<bool>& recPath,
+                     vector<vector<int>>& adj) {
         vis[src] = true;
         recPath[src] = true;
-        for(auto& edge: edges){
-            int u = edge[1];
-            int v = edge[0];
-
-            if(u==src){
-                //neighbour is visited?
-                if(!vis[v]){
-                    if(isCyclicDFS(v, vis, recPath, edges)){
-                        return true;
-                    }
-                }else if(recPath[v]){
+        for (int v : adj[src]) {
+            // neighbour is visited?
+            if (!vis[v]) {
+                if (isCyclicDFS(v, vis, recPath, adj)) {
                     return true;
                 }
+            } else if (recPath[v]) {
+                return true;
             }
         }
         recPath[src] = false;
         return false;
     }
+
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
         vector<bool> vis(n, false);
         vector<bool> recPath(n, false);
-        for(int i=0; i<n; i++){
-            if(!vis[i]){
-                if(isCyclicDFS(i, vis, recPath, prerequisites)){
+
+        vector<vector<int>> adj(n);
+        for (const auto& pre : prerequisites) {
+            adj[pre[1]].push_back(pre[0]);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                if (isCyclicDFS(i, vis, recPath, adj)) {
                     return false;
                 }
             }
         }
 
         return true;
-        
     }
 };
