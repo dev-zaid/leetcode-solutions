acb4b36823b204fc069981f698fc9f605ba99d58
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-04-12 16:02:24 +0400

    Time: 0 ms (100%), Space: 12.3 MB (27.86%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -21,28 +21,17 @@ public:
 
 class Solution {
 public:
-    unordered_map<Node* , Node*> mp;
+    unordered_map<Node*, Node*> mp;
 
     Node* cloneGraph(Node* node) {
-        if(node==NULL) return NULL;
+        if (node == NULL)
+            return NULL;
 
-        // creating a clone of the start node
-        mp[node] = new Node(node->val);
 
-        queue<Node*> q;
-        q.push(node);
-
-        while(!q.empty()){
-            Node* curr = q.front();
-            q.pop();
-
-            for(Node* neigh : curr->neighbors){
-                if(mp.find(neigh) == mp.end()){
-                    mp[neigh] = new Node(neigh->val, {}); //create copy
-                    q.push(neigh);
-                }
-
-                mp[curr]->neighbors.push_back(mp[neigh]); // add vertex to the cloned node
+        if (mp.find(node) == mp.end()) {         // if not visited
+            mp[node] = new Node(node->val, {}); // create copy
+            for (Node* neigh : node->neighbors) {
+                mp[node]->neighbors.push_back(cloneGraph(neigh));
             }
         }
 
