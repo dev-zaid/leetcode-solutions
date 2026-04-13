067b9cc5a7726e5bfe36c4dca4b1ac55a0b9b857
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-04-13 18:56:00 +0400

    Time: 35 ms (48.97%), Space: 21.9 MB (47.88%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    int solver(int n, int target, vector<int>& arr, vector<vector<int>>& dp) {
+    int solver(int n, int target, vector<int>& arr, vector<int>& prev, vector<int>& curr) {
         // Tabulization
         //  base case
         for (int i = 1; i <= target; i++) {
             if (i % arr[0] == 0)
-                dp[0][i] = i / arr[0];
+                prev[i] = i / arr[0];
             else
-                dp[0][i] = 1e9;
+                prev[i] = 1e9;
         }
 
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < target + 1; j++) {
-                int notTake = 0 + dp[i - 1][j];
+                int notTake = 0 + prev[j];
                 int take = 1e9;
 
                 if (j >= arr[i]) {
-                    take = 1 + dp[i][j - arr[i]];
+                    take = 1 + curr[j - arr[i]];
                 }
 
-                dp[i][j] = min(notTake, take);
+                curr[j] = min(notTake, take);
             }
+
+            prev = curr;
         }
 
-        int ans = dp[n - 1][target];
+        int ans = prev[target];
         return ans < 1e9 ? ans : -1;
     }
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
 
-        vector<vector<int>> dp(n, vector<int>(amount + 1, 0));
+        vector<int> prev(amount + 1, 0);
+        vector<int> curr(amount + 1, 0);
 
-        return solver(n, amount, coins, dp);
+        return solver(n, amount, coins, prev, curr);
     }
 };
