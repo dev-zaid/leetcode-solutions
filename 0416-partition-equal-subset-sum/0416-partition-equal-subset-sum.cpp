e9d02899952a297230a05ae94dd6021b21ee279e
Mohd Zaid <36443577+dev-zaid@users.noreply.github.com>
2026-04-13 17:31:55 +0400

    Time: 151 ms (34.07%), Space: 17.3 MB (68.36%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    bool targetSubarraySum(int idx, int target, vector<int>& arr, vector<vector<int>> &dp){
-        if(target==0) return true;
-        if(idx == 0){
-            return target == arr[0];
-        }
-        if(dp[idx][target] != -1) return dp[idx][target];
-
-        // idx-1, 
-        bool notTake = targetSubarraySum(idx-1, target, arr, dp);
-
-        bool take = false;
-
-        if(target >= arr[idx]){
-            take = targetSubarraySum(idx-1, target-arr[idx], arr, dp);
-        }
-
-        return dp[idx][target] = take | notTake;
-    }
 
     bool canPartition(vector<int>& nums) {
         int totalSum = 0;
-        for(auto &i: nums) totalSum += i;
+        for (auto& i : nums)
+            totalSum += i;
 
-        if(totalSum % 2 != 0) return false;
+        if (totalSum % 2 != 0)
+            return false;
 
         int n = nums.size();
-        int target = totalSum/2;
+        int target = totalSum / 2;
 
-        vector<vector<int>> dp(n, vector<int>(target+1, -1));
+        vector<vector<bool>> dp(n, vector<bool>(target + 1, 0));
 
-        return targetSubarraySum(n-1, target, nums, dp);
+        for (int i = 0; i < n; i++)
+            dp[i][0] = true; // target = 0;
+        if (nums[0] <= target)
+            dp[0][nums[0]] = true;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= target; j++) {
+                bool notTake = dp[i - 1][j];
+                bool take = false;
+
+                if (j >= nums[i]) {
+                    take =
+                        dp[i - 1][j - nums[i]];
+                }
+
+                dp[i][j] = take | notTake;
+            }
+        }
+
+        return dp[n - 1][target];
     }
 };
